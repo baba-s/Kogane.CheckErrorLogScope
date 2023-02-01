36f2abd00dd58a1bf413294fd7ed3cc3af7653ec
baba-s <shotababa1206@gmail.com>
2023-02-01 21:04:37 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Kogane.CheckErrorLogScope
+# Kogane Check Error Log Scope
+
+エラーや例外、アサートが発生したかどうかを確認できるスコープ
+
+## 使用例
+
+```cs
+using Kogane;
+using UnityEngine;
+
+public sealed class Example : MonoBehaviour
+{
+    private void Start()
+    {
+        using var scope = new CheckErrorLogScope();
+
+        Debug.LogError( "ピカチュウ" );
+
+        if ( scope.IsError )
+        {
+            // ...
+        }
+    }
+}
+```
---
+++ Runtime/CheckErrorLogScope.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kogane
+{
+    /// <summary>
+    /// Assert や Debug.LogError、Exception が発生したかどうかを確認できるクラス
+    /// </summary>
+    public sealed class CheckErrorLogScope : IDisposable
+    {
+        //================================================================================
+        // プロパティ
+        //================================================================================
+        public bool IsError { get; private set; }
+
+        public bool IsSuccess => !IsError;
+
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public CheckErrorLogScope()
+        {
+            Application.logMessageReceivedThreaded += OnLogMessageReceivedThreaded;
+        }
+
+        /// <summary>
+        /// 破棄します
+        /// </summary>
+        public void Dispose()
+        {
+            Application.logMessageReceivedThreaded -= OnLogMessageReceivedThreaded;
+        }
+
+        /// <summary>
+        /// ログ出力された時に呼び出されます
+        /// </summary>
+        private void OnLogMessageReceivedThreaded
+        (
+            string  condition,
+            string  trace,
+            LogType type
+        )
+        {
+            if ( IsError ) return;
+
+            IsError = type is
+                    LogType.Assert or
+                    LogType.Error or
+                    LogType.Exception
+                ;
+
+            if ( !IsError ) return;
+
+            Application.logMessageReceivedThreaded -= OnLogMessageReceivedThreaded;
+        }
+    }
+}
